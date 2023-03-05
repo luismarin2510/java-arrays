600c62bca239a25c160800d8a5e8df98b0a10fcf
Luismarin2510 <opraedu@gmail.com>
2023-03-05 18:59:14 -0500

    creacion de objeto con get set y contructor

--- src/lista_de_objetos.java
+++ src/lista_de_objetos.java
@@ -1,10 +1,29 @@
 
 public class lista_de_objetos {
 
-		
-		private String nombre;
-		private int tiempo;
-		
-	
+	private String nombre;
+	private int tiempo;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	public lista_de_objetos(String nombre, int tiempo) {
+		super();
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+	}
 
 }
