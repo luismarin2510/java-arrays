721ebe6437ff276c40cab6442c6b7670f6ef5e0a
Luismarin2510 <opraedu@gmail.com>
2023-03-05 17:43:06 -0500

    primeros arrays

---
+++ src/Lectura_arrays.java
@@ -0,0 +1,9 @@
+
+public class Lectura_arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/arraylista.java
+++ src/arraylista.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
 
 public class arraylista {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String Variable1 = "Ejemplo1";
+		String Variable2 = "Ejemplo2";
+		String Variable3 = "Ejemplo3";
+		String Variable4 = "Ejemplo4";
+
+		ArrayList<String> lista = new ArrayList();
+
+		lista.add(Variable1);
+		lista.add(Variable2);
+		lista.add(Variable3);
+		lista.add(Variable4);
+
+		System.out.println(lista);
+
+		lista.remove(2);
+		System.out.println(lista);
+
+		lista.set(1, "ejemplo alterado");
+		System.out.println(lista);
 
 	}
 
